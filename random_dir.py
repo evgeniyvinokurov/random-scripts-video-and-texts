5e1f8537a35eacf4048193cc216a3f8791b5c292
Evgeniy Vinokurov <evgeniy.vinokuroff@yandex.ru>
2026-02-13 08:51:12 +0300

    updates sccripts

--- random_dir.py
+++ random_dir.py
@@ -6,31 +6,38 @@ from lib.eightball import EightBall
 
 dir = "/media/evgenii/TOSHIBA EXT"
 
-print("enter salts:")
-salts = str(input())
-
-
-with open('dirs.txt', 'r') as f:
-    dirs = f.readlines()
         
 # print(dirs)
 count = 5
 one_dir = ""
-e81 = EightBall(salts)
+mode = "8balll"
+e81 = None
+
+if mode == "8ball":
+    print("enter salts:")
+    salts = str(input())
+    e81 = EightBall(salts)
+
+with open('dirs.txt', 'r') as f:
+    dirs = f.readlines()
+
+def get(mode, ar):
+    if mode == "8ball":               
+        return e81.getOneByEightBall(ar)
+    else:
+        return random.choice(ar)
 
 while True:
     if len(dirs) > 0:
-        one_dir = e81.getOneByEightBall(dirs)
+        one_dir = get(mode, dirs)
     else:
-        dirs = Usefull.find_all_dirs(dir)              
-        one_dir = e81.getOneByEightBall(dirs)
-        
+        dirs = Usefull.find_all_dirs(dir)        
         # one_dir = random.choice(dirs)
 
         with open('dirs.txt', 'w') as f:
             for i in dirs:
-                f.write(i + '\r\n')
-
+                f.write(i + '\r\n')            
+        one_dir = get(mode, dirs)
     print(one_dir)
 
     count = count - 1
